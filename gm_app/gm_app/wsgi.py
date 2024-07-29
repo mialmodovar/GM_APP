571f4232a049075af450ca7f1e735d99956e40dd
Miguel Almodovar <53450442+mialmodovar@users.noreply.github.com>
2024-07-29 17:27:08 +0000

    m

--- gm_app/gm_app/wsgi.py
+++ gm_app/gm_app/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nutritiongpt.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gm_app.settings')
 
 application = get_wsgi_application()
