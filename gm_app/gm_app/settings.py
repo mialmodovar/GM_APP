4c4076419563352e58c2268b5125e91c9fccc581
Varun_129809 <31147848+vart90@users.noreply.github.com>
2024-07-30 13:47:08 +0100

    new-settings.py

--- gm_app/gm_app/settings.py
+++ gm_app/gm_app/settings.py
@@ -81,14 +81,16 @@ WSGI_APPLICATION = 'gm_app.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'vs-postgres',  # Replace with the name of your PostgreSQL database
+        'USER': 'varun129809',
+        'PASSWORD': 'Postgres98!',  # Replace with your actual password
+        'HOST': 'vs-postgres.postgres.database.azure.com',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
