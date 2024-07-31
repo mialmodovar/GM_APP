55bc83e7dab9a6906473b616fe93d072306408c6
Miguel Almodovar <53450442+mialmodovar@users.noreply.github.com>
2024-07-31 14:49:00 +0000

    d

--- gm_app/gm_app/settings.py
+++ gm_app/gm_app/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-49pz7d6inn@x0raym0ylbj3t*uul1hug408qfbm79!x(#w2z33'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['https://gmapp-production.up.railway.app']
 
